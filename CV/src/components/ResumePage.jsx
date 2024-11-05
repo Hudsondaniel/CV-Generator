9f5f5bf7465f65477addf0b8ffd5a67a94329182
Hudson Daniel <60006022+Hudsondaniel@users.noreply.github.com>
2024-11-05 22:52:30 +0530

    auto scroll is successful

--- CV/src/components/ResumePage.jsx
+++ CV/src/components/ResumePage.jsx
@@ -3,8 +3,7 @@ import '../styles/ResumePage.css'
 export default function ResumePage(){
     return(
         <div className="resume-page">
-            <h1>Resume</h1>
-            <p>This is a sample resume page. You can replace this content with your actual resume.</p>
+            
         </div>
     )
 }
