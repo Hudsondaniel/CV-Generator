88ac0d590c7db5b0c9f730de585d657e8d832fd4
Hudson Daniel <60006022+Hudsondaniel@users.noreply.github.com>
2024-11-05 22:16:02 +0530

    Detail section complete for desktop and few more things like main section added

--- CV/src/App.jsx
+++ CV/src/App.jsx
@@ -5,6 +5,7 @@ import ShortBio from './components/ShortBio';
 import Experience from './components/Experience';
 import Education from './components/Education';
 import Skills from './components/Skills';
+import ResumePage from './components/ResumePage';
 import './App.css'
 
 
@@ -15,11 +16,18 @@ function App() {
     <>
       < NavBar/>
       < DetailSection/>
-      < YourDetails/>
-      < ShortBio/>
-      < Experience/>
-      < Education/>
-      < Skills/>
+      <div className="main-section">
+        <div className="left-section">
+          < YourDetails/>
+          < ShortBio/>
+          < Experience/>
+          < Education/>
+          < Skills/>
+        </div>
+        <div className="right-section">
+          < ResumePage/>
+        </div>
+      </div>
     </>
   )
 }
