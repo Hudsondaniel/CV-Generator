81b957e4f3b3350093de9cf7afe554cd6d5946c7
Hudson Daniel <60006022+Hudsondaniel@users.noreply.github.com>
2024-11-05 16:06:11 +0530

    Working on the nav bar

--- CV/src/components/NavBar.jsx
+++ CV/src/components/NavBar.jsx
@@ -1,5 +1,13 @@
-import '../styles/NavBar.css'
+import '../styles/NavBar.css';
+import profilePic from '../assets/Img/Nav-Profile-pic.jpg'; 
 
-export default function NavBar(){
-    
+export default function NavBar() {
+    return (
+        <nav className="navbar">
+            <h1 className='nav-heading'>CV Builder</h1>
+            <div className='profile-pic-nav'>
+                <img src={profilePic} alt="Profile" /> 
+            </div>
+        </nav>
+    );
 }
