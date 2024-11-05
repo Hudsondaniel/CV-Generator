69d78bfe2a5a5bcf2d5622fb1e512e265f0975da
Hudson Daniel <60006022+Hudsondaniel@users.noreply.github.com>
2024-11-05 18:52:17 +0530

    Getting the buttons to be even sized

---
+++ CV/src/components/DetailSection.jsx
@@ -0,0 +1,13 @@
+import '../styles/DetailSection.css';
+
+export default function DetailSection(){
+    return (
+        <div className='buttons-details'>
+            <button className='your-details'>Your Details</button>
+            <button className='short-bio'>Short Bio</button>
+            <button className='experience'>Experience</button>
+            <button className='education'>Education</button>
+            <button className='skills'>Skills</button>
+        </div>
+    )
+}
