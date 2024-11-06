3a69290ca5be061b56e3e532af5117718ae59e82
Hudson Daniel <60006022+Hudsondaniel@users.noreply.github.com>
2024-11-06 17:55:50 +0530

    Rendering the item that the button selects is done

--- CV/src/App.jsx
+++ CV/src/App.jsx
@@ -7,24 +7,39 @@ import Education from './components/Education';
 import Skills from './components/Skills';
 import ResumePage from './components/ResumePage';
 import './App.css'
+import { useState } from 'react';
 
 
 function App() {
+  const [activeSection, setActiveSection] = useState(null);
+
+  const renderActiveComponent = () =>{
+    switch(activeSection){
+      case 'yourDetails':
+        return <YourDetails/>
+      case 'shortBio':
+        return <ShortBio/>
+      case 'experience':
+        return <Experience/>
+      case 'education':
+        return <Education/>
+      case'skills':
+        return <Skills/>
+      default:
+        return <YourDetails/>
+    }
+  }
 
 
   return (
     <>
       <div className="nav-section">
         < NavBar/>
-        < DetailSection/>
+        < DetailSection setActiveSection={setActiveSection}/>
       </div>
       <div className="main-section">
         <div className="left-section">
-          < YourDetails/>
-          < ShortBio/>
-          < Experience/>
-          < Education/>
-          < Skills/>
+          {renderActiveComponent()}
         </div>
         <div className="right-section">
           < ResumePage/>
@@ -35,3 +50,4 @@ function App() {
 }
 
 export default App
+
--- CV/src/components/DetailSection.jsx
+++ CV/src/components/DetailSection.jsx
@@ -1,13 +1,29 @@
 import '../styles/DetailSection.css';
+import PropTypes from 'prop-types';
 
-export default function DetailSection(){
+export default function DetailSection({ setActiveSection }){
     return (
         <div className='buttons-details'>
-            <button className='your-details'>Your Details</button>
-            <button className='short-bio'>Short Bio</button>
-            <button className='experience'>Experience</button>
-            <button className='education'>Education</button>
-            <button className='skills'>Skills</button>
+            <button onClick={()=>setActiveSection('yourDetails')} className='your-details'>
+                Your Details
+            </button>
+            <button onClick={()=> setActiveSection('shortBio')} className='short-bio'>
+                Short Bio
+            </button>
+            <button onClick={()=> setActiveSection('experience')} className='experience'>
+                Experience
+            </button>
+            <button onClick={()=> setActiveSection('education')} className='education'>
+                Education
+            </button>
+            <button onClick={()=> setActiveSection('skills')} className='skills'>
+                Skills
+            </button>
         </div>
     )
 }
+
+DetailSection.prototype ={
+    setActiveSection: PropTypes.func.isRequired,
+}
+
