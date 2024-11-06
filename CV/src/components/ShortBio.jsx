7c078ee63795b837f7609eed903ffd226c462572
Hudson Daniel <60006022+Hudsondaniel@users.noreply.github.com>
2024-11-06 18:13:02 +0530

    Upload design files and start a new section

--- CV/src/components/ShortBio.jsx
+++ CV/src/components/ShortBio.jsx
@@ -1,5 +1,10 @@
 import '../styles/ShortBio.css'
 
 export default function ShortBio(){
-    
+    return(
+        <div className="short-bio-section">
+            <h2>Short Bio</h2>
+            <p>Your short bio goes here.</p>
+        </div>
+    )
 }
