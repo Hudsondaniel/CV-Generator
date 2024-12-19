5980cd4cc9317eef393f58dec3c722dc2e521cda
Hudson Daniel <60006022+Hudsondaniel@users.noreply.github.com>
2024-12-19 15:26:30 +0530

    Zustand is great

--- CV/src/components/ResumePage.jsx
+++ CV/src/components/ResumePage.jsx
@@ -1,9 +1,18 @@
 import '../styles/ResumePage.css'
+import useDetailsStore from '../stores/useDetailsStore'
 
 export default function ResumePage(){
+    const { image, jobTitle, firstName, lastName, email, phoneNumber, address, 
+        setImage, setJobTitle, setFirstName, setLastName, setEmail, setPhoneNumber, setAddress} = useDetailsStore();
+
+    
     return(
         <div className="resume-page">
-            
+
+                <h1>{firstName} {lastName}</h1>
+                <h2>{jobTitle}</h2>
+
+        
         </div>
     )
 }
